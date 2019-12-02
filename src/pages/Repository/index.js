16f65f255a8f2b4fe2e971e5631c2a9ee1aef802
Thiago morais <suigvo@hotmail.com>
2019-12-02 16:28:57 -0300

    commit ajustes de PropTypes

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-function Repository({ match }) {
-  return <h1>Repository:{decodeURIComponent(match.params.repository)}</h1>;
+import api from '../../services/api';
+
+class Repository extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      repository: {},
+      issues: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      loading: false,
+      repository: repository.data,
+      issues: issues.data,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    return <h1>Repository</h1>;
+  }
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repository: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Repository;
